b8e84e301c1d59d8cee3a4348fa43db6e28b75ea
PLWEP <langgeng86@gmail.com>
2024-01-03 12:17:40 +0700

    nambah register page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:bk_lapor_book/firebase_options.dart';
+import 'package:bk_lapor_book/pages/register_page.dart';
+import 'package:bk_lapor_book/pages/splash_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -7,20 +9,17 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MainApp());
-}
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  runApp(MaterialApp(
+    title: 'Lapor Book',
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const SplashPage(),
+      // '/login': (context) => LoginPage(),
+      '/register': (context) => const RegisterPage(),
+      // '/dashboard': (context) => const DashboardPage(),
+      // '/add': (context) => AddFormPage(),
+      // '/detail': (context) => DetailPage(),
+    },
+  ));
 }
