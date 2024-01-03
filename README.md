7f6285aff8f877d29453508cfef606d40fd85a5b
PLWEP <langgeng86@gmail.com>
2024-01-03 11:42:12 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bk_lapor_book
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/bk_lapor_book/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bk_lapor_book
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
